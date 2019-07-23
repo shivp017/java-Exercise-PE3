12f3444033f1da100efcdd5ed0d3ce65cd19b143
shiv.prakash <shiv.prakash@cgi.com>
2019-07-23 11:39:05 +0530

    updated Exception

---
+++ src/main/java/com/stackroute/pe3/Exception.java
@@ -0,0 +1,34 @@
+package com.stackroute.pe3;
+
+public class Exception {
+    public static void main(String[] args) {
+//        NegativeArraySizeException
+        try{
+            int arr[]=new int[-3];
+        }catch(NegativeArraySizeException e){
+            System.out.println(e);
+        }
+
+//        IndexOutOfBoundsException
+        try{
+            int arr[]=new int[3];
+            arr[3]=30;
+        }catch(IndexOutOfBoundsException e){
+            System.out.println(e);
+        }
+
+//        NullPointerException
+        try{
+            int arr[]=null;
+            int length=arr.length;
+        }catch(NullPointerException e){
+            System.out.println(e);
+        }
+    }
+
+
+
+}
+
+
+
---
+++ src/main/test/java/com/stackroute/pe3/ExceptionTest.java
@@ -0,0 +1,8 @@
+package com.stackroute.pe3;
+
+import static org.junit.Assert.*;
+
+public class ExceptionTest {
+
+
+}
